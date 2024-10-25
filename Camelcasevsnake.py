fc09152c4c20139c2fcb6ce2f01a14f3f22708d4
Musenga-12 <pmusenga567@gmail.com>
2024-10-25 15:34:56 +0200

    Update Camelcasevsnake.py

--- Camelcasevsnake.py
+++ Camelcasevsnake.py
@@ -1,15 +1,10 @@
-def camel_to_snake(camel_case):
-    snake_case = ""
-    for char in camel_case:
-        if char.isupper():
-            snake_case += "_" + char.lower()
-        else:
-            snake_case += char
-    # Remove leading underscore if the input starts with an uppercase
-    if snake_case[0] == "_":
-        snake_case = snake_case[1:]
+def camel_case(camel_case_input):
+    snake_case =""
+    for char in camel_case_input :
+        if char.isupper () :
+            snake_case +="_"+char.lower ()
+        else :
+            snake_case +=char 
     return snake_case
-
-# Get input from the user
-camel_case_input = input("Enter the variable name in camelCase: ")
-print("Variable name in snake_case:", camel_to_snake(camel_case_input))
+camel_case_input =input("\nenter the name:")
+print (f"The snake case is :{camel_case (camel_case_input )}")
